13a3df13955bd5b8a39c825cd4bc90ee08b46394
Sergio López <sergiolf96@gmail.com>
2019-04-26 13:02:19 +0200

    Dataset ready. CNN not working

--- Dockerfile
+++
@@ -1,6 +0,0 @@
-FROM continuumio/miniconda3
-RUN apt-get update
-WORKDIR home/
-RUN git clone https://github.com/sergiolopezdf/MusicRecognisionNeuralNetworks
-WORKDIR MusicRecognisionNeuralNetworks
-RUN conda env create -f neuralNetworksEnv.yml
--- NNTestV2_numpy.py
+++ NNTestV2_numpy.py
@@ -1,7 +1,7 @@
 from keras.layers import Dense, Flatten
 from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, GlobalMaxPooling2D
 from keras.models import Sequential
-from numpy import load, array, vstack, empty, append
+from numpy import load, array, vstack, empty, append, dstack, hstack, concatenate, zeros
 from pathlib import Path
 import os
 from sklearn.model_selection import train_test_split
@@ -12,122 +12,112 @@ from sklearn.model_selection import train_test_split
 
 path = 'dataset'
 
+# Get files to add
 files = os.listdir(path)
+n_files = len(files)
 
-dataset = load(Path(path) / 'init.npy')
+# Init array
+dataset = zeros((n_files, 15, 14))
 
+# x - n_files
+# y - mfcc
+# z - value
 
-for mfcc_file in files:
-
-    # If it's init file, skip it
-    if mfcc_file == 'init.npy':
-        continue
+# Filling in the 3D array
+for i in range(0, n_files):
 
     # Load current file
-    file = Path(path) / mfcc_file
+    file = Path(path) / files[i]
     array = load(file)
 
-    # Init has this dimension. If dataset shape is 1, 14; it's empty
-    s = (1, 14)
+    dataset[i] = array
 
-    if dataset.shape == s:
-        dataset = array
-        continue
-
-    dataset = vstack((dataset, array))
-
-print(dataset)
-#
-# x = []
-# y = []
-
-# for index in dataset:
-#     x.append(index[0:12])
-#     y.append(index[13])
-#
-# # To numpy
-# x = array(x)
-# y = array(y)
-#
-# print(x)
+# Split into x and y
+x = dataset[0:dataset.shape[0], 0:15, 0:13]
+y = dataset[0:dataset.shape[0], 0:15, [13]]
 
 
 
+# # Splitting the DF to get 80/20 elements
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= (0.2), random_state=1, shuffle=True)
 
-#
-# # # Splitting the DF to get 80/20 elements
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size= (0.2), random_state=1, shuffle=True)
-#
-# height = 99
-# width = 12
-# depth = 1
-#
-# input_shape = (height, width, depth)
-#
-#
-# x_train = x_train.reshape(1, 99, 12, 1)
-# x_test = x_test.reshape(1, 25, 12, 1)
-#
-# model = Sequential()
-#
-# model.add(Conv2D(32, (3, 3), padding="same", input_shape=input_shape))
-#
-# model.add(Activation("relu"))
-#
-# model.add(Conv2D(32, (3, 3), padding="same"))
-#
-# model.add(Activation("relu"))
-#
-# model.add(MaxPooling2D(pool_size=(3, 3)))
-#
-# model.add(Dropout(0.25))
-#
-# model.add(Conv2D(64, (3, 3), padding="same"))
-#
-# model.add(Activation("relu"))
-#
-# model.add(Conv2D(64, (3, 3), padding="same"))
-#
-# model.add(Activation("relu"))
-#
-# model.add(MaxPooling2D(pool_size=(3, 3)))
-#
-# model.add(Dropout(0.25))
-#
-# model.add(Conv2D(128, (3, 3), padding="same"))
-#
-# model.add(Activation("relu"))
-#
-# model.add(Conv2D(128, (3, 3), padding="same"))
-#
-# model.add(Activation("relu"))
-#
-# model.add(MaxPooling2D(pool_size=(3, 3)))
-#
-# model.add(Dropout(0.25))
-#
-# model.add(Conv2D(256, (3, 3), padding="same"))
-#
-# model.add(Activation("relu"))
-#
-# model.add(Conv2D(256, (3, 3), padding="same"))
-#
-# model.add(Activation("relu"))
-#
-# model.add(GlobalMaxPooling2D())
-#
-# model.add(Flatten())
-#
-# model.add(Dropout(0.5))
-#
-# model.add(Activation("softmax"))
-#
-# model.fit(x_train, y_train,
-#           epochs=2,
-#           validation_data=(x_test, y_test),
-#           )
-#
-# score = model.evaluate(x_test, y_test, verbose=0)
-# print('Test loss:', score[0])
-# print('Test accuracy:', score[1])
-# print(model.summary())
+# Each file has 15 seconds == 15 samples
+height = 15
+
+# 13 MFCC per sample
+width = 13
+
+# 1 plane
+depth = 1
+
+input_shape = (height, width, depth)
+
+
+
+x_train = x_train.reshape(x_train.shape[0], height, width, depth)
+x_test = x_test.reshape(x_test.shape[0], height, width, depth)
+
+
+model = Sequential()
+
+model.add(Conv2D(32, (3, 3), padding="same", input_shape=input_shape))
+
+model.add(Activation("relu"))
+
+model.add(Conv2D(32, (3, 3), padding="same"))
+
+model.add(Activation("relu"))
+
+model.add(MaxPooling2D(pool_size=(3, 3)))
+
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding="same"))
+
+model.add(Activation("relu"))
+
+model.add(Conv2D(64, (3, 3), padding="same"))
+
+model.add(Activation("relu"))
+
+model.add(MaxPooling2D(pool_size=(3, 3)))
+
+model.add(Dropout(0.25))
+
+model.add(Conv2D(128, (3, 3), padding="same"))
+
+model.add(Activation("relu"))
+
+model.add(Conv2D(128, (3, 3), padding="same"))
+
+model.add(Activation("relu"))
+
+model.add(MaxPooling2D(pool_size=(3, 3)))
+
+model.add(Dropout(0.25))
+
+model.add(Conv2D(256, (3, 3), padding="same"))
+
+model.add(Activation("relu"))
+
+model.add(Conv2D(256, (3, 3), padding="same"))
+
+model.add(Activation("relu"))
+
+model.add(GlobalMaxPooling2D())
+
+model.add(Flatten())
+
+model.add(Dropout(0.5))
+
+model.add(Activation("softmax"))
+
+model.fit(x_train, y_train,
+          epochs=2,
+          validation_data=(x_test, y_test),
+          )
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+print(model.summary())
